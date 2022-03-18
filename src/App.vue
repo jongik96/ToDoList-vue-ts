14fa882bc565f3e12b08ee162a6bbf828c143a99
jongik96 <pji3504@naver.com>
2022-03-18 21:43:24 +0900

    create addTodo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue Todo With TypeScript</h1>
+    <TodoInput
+      :item="todoText"
+      @input="updateTodoText"
+      @add="addTodoItem"
+    ></TodoInput>
+    <!-- <input type="text" v-model="todoText"> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+  methods: {
+    updateTodoText(value: any) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    },
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button @click="addTodo" type="button">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  // props 에 대한 type 유효성 검사
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(event: InputEvent) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+    addTodo() {
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
